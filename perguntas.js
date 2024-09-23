16c1c42877fa961ba9454b9faaa3660ae4f3b639
3mimii <144124125+3mimii@users.noreply.github.com>
2024-09-23 08:30:45 -0300

    4, 5 e 6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Borboletas',
+    'quantos dias pode durar a metamorfose de largata em borboleta?',
+    'dura de dez a quinze dias'
+)
+
+criaCartao(
+    'Borboletas',
+    'quantas patas possuem as borboetas?',
+    'possuem 6 patas 3 de cada lado'
+)
+
+criaCartao(
+    'Borboletas',
+    'por que as borboletas possuem seus esqueletos do lado de fora do corpo? ',
+    'para proteger e manter a água dentro de seus corpos para que elas não sequem'
+)
