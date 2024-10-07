249008a600d7bd147dc3bbcdcc451a434d0a18ee
3mimii <144124125+3mimii@users.noreply.github.com>
2024-10-07 08:02:34 -0300

    finalizado

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,9 @@ criaCartao(
     'por que as borboletas possuem seus esqueletos do lado de fora do corpo? ',
     'para proteger e manter a água dentro de seus corpos para que elas não sequem'
 )
+criaCartao(
+    'Borboletas',
+    'O que a borboleta significa? ',
+    'A borboleta é considerada o símbolo da transformação, da felicidade, da beleza, da inconstância, da eferemidade da natureza e da renovação.'
+)
+
